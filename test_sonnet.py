6feb7bcd4cfa13ba4bbf9d8ff4929e7faa9343d6
Erik Reed <ereed.uw@gmail.com>
2015-10-11 16:43:58 -0700

    style

--- test_sonnet.py
+++ test_sonnet.py
@@ -2,7 +2,6 @@ import sonnet
 
 
 class TestSonnet(object):
-
     def test_rhyming(self):
         assert sonnet.check_rhyme('cool', 'fool')
         assert sonnet.check_rhyme('cat', 'hat')
@@ -11,15 +10,12 @@ class TestSonnet(object):
         assert not sonnet.check_rhyme('fabulous', 'cat')
 
     def test_multi_line_rhyming(self):
-        assert not sonnet.validate_sonnet_rhyming(
-            [
-                'unique new york',
-                'how now brown cow'
-            ]
-        )
-        assert sonnet.validate_sonnet_rhyming(
-            [
-                'rhyming scheme',
-                'steel beam'
-            ]
-        )
+        assert not sonnet.validate_sonnet_rhyming([
+            'unique new york',
+            'how now brown cow'
+        ])
+
+        assert sonnet.validate_sonnet_rhyming([
+            'rhyming scheme',
+            'steel beam'
+        ])
